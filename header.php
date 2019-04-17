8858d5e8bd9ef09952b0e8901906372da25062dd
zcoder83 <dusrksl@gmail.com>
2019-04-17 23:26:03 +0900

    2019.04.17 version 2.0

--- header.php
+++ header.php
@@ -8,6 +8,10 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Roboto+Slab|Sanchez" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:800" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script
+  src="https://code.jquery.com/jquery-3.4.0.js"
+  integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
+  crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class($class); ?> onload="myFunction()">
